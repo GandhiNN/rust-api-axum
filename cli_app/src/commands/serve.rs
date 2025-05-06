fc47bf66a5c9ceaedd23da718c53b3f12dd535a3
Ngakan Gandhi <ngandhi@pmintl.net>
2025-05-06 12:26:08 +0700

    feat: add more examples

--- cli_app/src/commands/serve.rs
+++ cli_app/src/commands/serve.rs
@@ -1,12 +1,21 @@
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command, value_parser};
 
 pub const COMMAND_NAME: &str = "serve";
 
 pub fn configure() -> Command {
-    Command::new(COMMAND_NAME).about("Start HTTP server")
+    Command::new(COMMAND_NAME).about("Start HTTP server").arg(
+        Arg::new("port")
+            .short('p')
+            .long("port")
+            .value_name("PORT")
+            .help("TCP port to listen on")
+            .default_value("8080")
+            .value_parser(value_parser!(u16)),
+    )
 }
 
-pub fn handle(_matches: &ArgMatches) -> anyhow::Result<()> {
-    println!("TBD: start the webserver on port ??? ");
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    let port: u16 = *matches.get_one("port").unwrap_or(&8080);
+    println!("TBD: start the webserver on port {} ", port);
     Ok(())
 }
